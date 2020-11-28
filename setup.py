033c81716cb6eefd7b4282064a15b56b4941eba6
Brendan Leo <brendan.leo@gmail.com>
2020-11-29 00:28:21 +1100

    Rename project to pybeacon

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='nswses_identity',
+    name='pybeacon',
     version='0.1.0',
     description='A library for retrieving credentials to interact with the Beacon frontend and API',
     author='Brendan Leo',
     license='MIT',
-    packages=find_packages(include=['nswses_identity']),
+    packages=find_packages(include=['pybeacon']),
     install_requires=[
         'requests-html',
     ],
